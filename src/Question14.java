35575ba9dc02b21bc7aa40eb700f82c83489a7f8
Demo <1012858388@qq.com>
2019-12-11 09:57:46 +0800

    10.31

---
+++ src/Question14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Question14 {
+    public int maxCut(int length){
+        if(length == 0){
+            return 0;
+        }
+        if(length == 1){
+
+        }
+        int[] product = new int[length + 1];
+        product[1] = 1;
+        product[2] = 2;
+        product[3] = 3;
+        int max = 0;
+        return max;
+    }
+}
